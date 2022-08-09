ab42f898935cc9f3811b71a68649be7aa5939d00
mominurislamrevature1 <mominur542@revature.net>
2022-08-09 15:41:54 -0700

    HTTP Client Assignment

---
+++ ClassLabProject/HttpClientApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientApp
+{
+      class Program
+    {
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetTodoItems();
+
+        }         
+
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync(
+                "http://api.icndb.com/jokes/random/3?firstName=Mark&lastName=Moore");
+               //"http://api.icndb.com/jokes/15?firstName=Sally&lastName=Hednrix");
+
+            Console.WriteLine(response);
+        }
+
+
+
+    }                          
+     
+}
