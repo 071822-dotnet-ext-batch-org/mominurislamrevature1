a98b3ef3f1cc1eecc9f66f2016a0e714507ab239
mominurislamrevature1 <mominur542@revature.net>
2022-08-18 22:37:49 -0700

    connected to azure and pull information From Db

---
+++ ReimbursementApp/Reimbursement/Controllers/ExpenseReimbursemtController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using BusinessLayer;
+
+
+namespace Reimbursement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseReimbursemtController : ControllerBase
+    {
+        
+        private readonly ReimbursementBusinessLayer _businessLayer;
+        public ExpenseReimbursemtController()
+        {
+            this._businessLayer = new ReimbursementBusinessLayer();
+
+
+        }
+
+
+
+        /// <summary>
+        //  Get all the pending requests
+        //  </summary>
+
+        [HttpGet("RequestsAsync")]  // get all requests
+        [HttpGet("RequestsAsync/{type}")]  // Get all of a type of requst
+       // [HttpGet("RequestsAsync/{type}/{id}")] // get all or a specific type and employee
+       // [HttpGet("RequestsAsync/{id}")] // get all of a specific employees requests
+
+        public async Task<ActionResult<List<ReimbursementReq>>> RequestsAsync(int type, Guid ? id)
+        {
+              
+                List<ReimbursementReq> requestList = await this._businessLayer.RequestsAsync(type);
+                return Ok(requestList);
+             
+        }
+
+        [HttpPut("UpdateRequestAsync")]
+        public async Task<ActionResult<UpdateReRequestDto>> updateRequestAsync(ApprovalDto approval)
+        {
+            if (ModelState.IsValid)
+            { 
+            // Send the ApprivalDto to business layer
+             UpdateReRequestDto approvedRequest = await this._businessLayer.UpdateRequestAsync(approval);
+            return Ok( approvedRequest);
+              }
+             else return Conflict(approval);//StatusCode(StatusCodes.Status409Conflict);
+        }
+
+    }
+}
---
+++ ReimbursementApp/RepositoryAccessLayer/ReimbursementRepoLayer.cs
@@ -0,0 +1,122 @@
+﻿using Models;
+using System.Data.SqlClient;
+//using System.Runtime.InteropServices;
+
+namespace RepositoryAccessLayer
+{
+    public class ReimbursementRepoLayer
+    {
+        public async Task<List<ReimbursementReq>> RequestsAsync(int type)
+
+        {
+            SqlConnection conn = new SqlConnection("Server=tcp:dbpro.database.windows.net,1433;Initial Catalog=Project1;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            using (SqlCommand command = new SqlCommand($"SELECT * FROM Reimbursement WHERE Status = @type", conn))
+            {
+
+                command.Parameters.AddWithValue("@type", type);
+                conn.Open();
+                SqlDataReader? ret = await command.ExecuteReaderAsync();
+                List<ReimbursementReq> rList = new List<ReimbursementReq>();
+                while (ret.Read())
+                {
+                    ReimbursementReq r = new ReimbursementReq((Guid)ret[0], (Guid)ret[1], ret.GetString(2), ret.GetDecimal(3), ret.GetInt32(4));
+                    rList.Add(r);
+                }
+                conn.Close();
+                return rList;
+
+            }
+        }
+        public async Task<UpdateReRequestDto> UpdateRequestAsync(Guid reimbursementId, int status)
+        {
+            SqlConnection conn = new SqlConnection("Server=tcp:dbpro.database.windows.net,1433;Initial Catalog=Project1;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            using (SqlCommand command = new SqlCommand($"UPDATE Reimbursement SET Status = @status WHERE ReimbursementID = @id", conn))
+            {
+                command.Parameters.AddWithValue("@id", reimbursementId);
+                command.Parameters.AddWithValue("status", status);
+                conn.Open();
+                int ret = await command.ExecuteNonQueryAsync();
+                if (ret == 1)
+                {
+                    conn.Close();
+                    //call the requestByid()
+                    // created 2 neew methods to get the request by 
+                    // ID And get the employee by id. Thes are methods that would be useful and reusable
+                    // Mark does not want to do 80% for the project
+
+                    //call the updaterequesttByid() . This method will join to return the employee name 
+                    // along with the relavent detail and return DTO
+
+
+                    UpdateReRequestDto urbi = await this.UpdateRequestByIdAsync(reimbursementId);
+                   // UpdateReRequestDto urbi = await this.UpdateRequestAsync(reimbursementId, status);
+
+                    // return.Close();
+                    return urbi;
+
+                }
+
+                conn.Close();
+                return null;
+            }
+
+        }
+
+        
+
+        public async Task<bool> IsManagerAsync(Guid employeeID)
+        {
+            SqlConnection conn = new SqlConnection("Server=tcp:dbpro.database.windows.net,1433;Initial Catalog=Project1;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            using (SqlCommand command = new SqlCommand($"SELECT IsManager FROM Employees WHERE EmployeeID = @id", conn))
+            {
+
+                command.Parameters.AddWithValue("@id", employeeID);
+                conn.Open();
+                SqlDataReader? ret = await command.ExecuteReaderAsync();
+                if (ret.Read() && ret.GetBoolean(0))
+
+                {
+                    conn.Close();
+                    return true;
+                }
+                conn.Close();
+                return false;
+
+            }
+        }
+
+
+
+
+
+        public async Task<UpdateReRequestDto> UpdateRequestByIdAsync(Guid reimbursementId)
+        {
+            SqlConnection conn = new SqlConnection("Server=tcp:dbpro.database.windows.net,1433;Initial Catalog=Project1;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            using (SqlCommand command = new SqlCommand($"SELECT ReimbursementID,FirstName, LastName, Status FROM Employees LEFT JOIN  Reimbursement ON EmployeeID = FK_EmployeeId WHERE ReimbursementID = @reimbursementId", conn))
+            {
+
+                command.Parameters.AddWithValue("@reimbursementId", reimbursementId);
+
+                conn.Open();
+                SqlDataReader? ret = await command.ExecuteReaderAsync();
+
+                if (ret.Read())
+                {
+
+                    UpdateReRequestDto r = new UpdateReRequestDto(ret.GetGuid(0), ret.GetString(1), ret.GetString(2), ret.GetInt32(3));
+                    conn.Close();
+
+                    return r;
+                }
+            }
+
+            conn.Close();
+            return null;
+        }
+
+
+    }
+
+} 
+
+ 
